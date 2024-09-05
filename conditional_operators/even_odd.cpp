28f5e5b084760a9866ecae842d1a64a74fb00a0d
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-05 23:02:55 +0530

    check even or odd

---
+++ conditional_operators/even_odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "enter number :";
+    cin >> n;
+
+    if (n%2==0) {
+        cout << "even\n";
+
+    }
+    else{
+        cout <<"odd\n";
+
+    }
+    return 0;
+}
