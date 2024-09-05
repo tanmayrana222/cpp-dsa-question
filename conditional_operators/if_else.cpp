f12ea8840cfb16888c4711bd30caebd4002ba9ba
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-05 22:56:16 +0530

    if_else

---
+++ conditional_operators/if_else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int age ;
+    cout << "enter age : " ;
+    cin >> age ;
+
+    if ( age >=18) {
+        cout <<"you can vote\n";
+
+    }
+    else{
+        cout <<"you can't vote\n";
+    }
+
+    return 0;
+
+} 
