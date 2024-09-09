71ee5ced16fe36234db061e89a32dfb9841662fa
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-10 00:40:44 +0530

    reverse loop

---
+++ Basics/10to1.cpp
@@ -0,0 +1,13 @@
+// printing number from 10 to 1 using while loop
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int i = 10 ;
+    while (i >=0 ) {
+        cout << i << endl;
+        i--;
+    }
+}
