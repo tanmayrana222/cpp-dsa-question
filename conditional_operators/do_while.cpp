851be9949da3b193e2efab9a4f32b1b7a23762ca
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-09 21:51:55 +0530

    1 to 13 (dowhile)

---
+++ conditional_operators/do_while.cpp
@@ -0,0 +1,18 @@
+//sum of odd numbers
+#include<iostream>
+using namespace std;
+
+int main ( ) {
+    int n = 13 ;
+    int i=1 ;
+
+    do {
+        cout << i << " ";
+        i++ ;
+    } while (i <= n);
+
+    cout << endl ;
+    return 0;
+
+    
+}
