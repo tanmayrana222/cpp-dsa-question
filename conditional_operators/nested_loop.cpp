0a4b34eeec9bf38a8cd89610f41e6e6d242fb0d3
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-10 23:20:50 +0530

    loop inside loop

---
+++ conditional_operators/nested_loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n = 10;
+    for (int i=5; i<=n; i++) {
+        int m = 10;
+        for ( int i=1; i<=m; i++) {
+            cout <<'*' ;
+        }
+        cout << endl;
+
+    }
+
+    return 0;
+
+
+}
