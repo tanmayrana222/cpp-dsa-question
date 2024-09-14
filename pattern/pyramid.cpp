f8ee61c28ca253ffceeb8425c9af67565b2d7c9c
tanmayrana222 <tanmayrana222@gmail.com>
2024-09-14 20:34:33 +0530

    pyramid triangle

---
+++ pattern/pyramid.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+
+int main ( ) {
+    int n = 5 ;
+
+    for (int i=0 ; i<n; i++) {
+        //space : n-i-1
+        for( int j=0; j<n-i-1; j++) {
+            cout << " ";
+        }
+
+        //nums 1 : i+1 
+        for (int j=1 ; j<=i+1; j++){
+            cout << j;
+        } 
+
+        //nums 2
+        for (int j=i; j>=1; j--) {
+            cout << j;
+        } 
+
+        cout << endl ;
+    }
+
+    return 0 ;
+
+
+
+}
